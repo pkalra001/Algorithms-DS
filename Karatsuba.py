e00aa1911a912c2fc8d89adf3779f2a9c11f9686
Piyush Kalra <kalrapiyush12@gmail.com>
2019-11-08 16:12:49 +0530

    third

--- Karatsuba.py
+++ Karatsuba.py
@@ -1,6 +1,20 @@
+import math
+
+
 def karatsuba(u, v):
-    x = str(u)
-    y = str(v)
-    n = len(max(x, y))
-    kg.kb.
+    x = str(max(u, v))
+    y = str(min(u, v))
+    n_half = math.ceil(len(x)/2)
+    a = x[:(n_half-1)]
+    b = x[(n_half-1):]
+    if len(y) <= n_half:
+
+
+
+
+
+
+
+
+
 
