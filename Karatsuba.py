08dd135b900807757b4f6ec24b78688692440a72
Piyush Kalra <kalrapiyush12@gmail.com>
2019-11-09 01:11:18 +0530

    Final1

--- Karatsuba.py
+++ Karatsuba.py
@@ -1,21 +1,25 @@
-import math
+import time
 
 
-def karatsuba(u, v):
-    x = str(max(u, v))
-    y = str(min(u, v))
-    n_half = math.floor(len(x) / 2)
-    a = int(x[:n_half])
-    b = int(x[n_half:])
-    if len(y) <= (n_half + 1):
-        c = 0
-        d = int(y)
-    else:
-        c = int(y[:(len(y) - (n_half + 1))])
-        d = int(y[(len(y) - (n_half + 1)):])
-
-    print(n_half)
-    print(a, b, c, d)
+def karat(u, v):
+    if len(str(u)) < 3 or len(str(v)) < 3:
+        return u * v
+    n = max(len(str(u)), len(str(v))) // 2
+    a = u // 10 ** n
+    b = u % 10 ** n
+    c = v // 10 ** n
+    d = v % 10 ** n
+    z0 = karat(a, c)
+    z1 = karat((a + b), (c + d))
+    z2 = karat(b, d)
+    return z0*(10**(2*n)) + (z1-z0-z2)*(10**n) + z2
 
 
-karatsuba(456786, 3365)
+g = 3141592653589793238462643383279502884197169399375105820974944592
+m = 2718281828459045235360287471352662497757247093699959574966967627
+t1 = time.time()
+print(g*m)
+print(time.time()-t1)
+t2 = time.time()
+print(karat(g, m))
+print(time.time()-t2)
