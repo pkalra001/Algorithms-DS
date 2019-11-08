52c10547f6a0dfcb400aaf3a97826a36d28ac089
Piyush Kalra <kalrapiyush12@gmail.com>
2019-11-08 17:40:11 +0530

    6

--- Karatsuba.py
+++ Karatsuba.py
@@ -7,15 +7,15 @@ def karatsuba(u, v):
     n_half = math.floor(len(x) / 2)
     a = int(x[:n_half])
     b = int(x[n_half:])
-    if len(y) <= n_half:
+    if len(y) <= (n_half + 1):
         c = 0
         d = int(y)
     else:
-        c = int(y[:(len(y) - n_half)])
-        d = int(y[(len(y) - n_half):])
+        c = int(y[:(len(y) - (n_half + 1))])
+        d = int(y[(len(y) - (n_half + 1)):])
 
     print(n_half)
     print(a, b, c, d)
 
 
-karatsuba(456, 334)
+karatsuba(456786, 3365)
