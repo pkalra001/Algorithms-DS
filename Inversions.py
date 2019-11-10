4d388d8f9c145769b35179e6e74212f32285c3cc
Piyush Kalra <kalrapiyush12@gmail.com>
2019-11-10 23:33:58 +0530

    working

--- Inversions.py
+++ Inversions.py
@@ -1,24 +1,21 @@
-n_inversions = 0
-
-
-def merge_sort(arr):
+def merge_sort_helper(arr, inv_count):
     if len(arr) == 1:
-        return
+        return inv_count
     a = arr[:(len(arr)//2)]
     b = arr[(len(arr)//2):]
-    merge_sort(a)
-    merge_sort(b)
-    merge(a, b, arr)
+    inv_count = merge_sort_helper(a, inv_count)
+    inv_count = merge_sort_helper(b, inv_count)
+    return merge(a, b, arr, inv_count)
 
 
-def merge(a, b, arr):
+def merge(a, b, arr, inv_count):
     i, j, k = 0, 0, 0
     while i < len(a) and j < len(b):
         if a[i] <= b[j]:
             arr[k] = a[i]
             i += 1
         else:
-            n_inversion = len(a) - i
+            inv_count += len(a) - i
             arr[k] = b[j]
             j += 1
         k += 1
@@ -30,11 +27,13 @@ def merge(a, b, arr):
         arr[k] = b[j]
         j += 1
         k += 1
+    return inv_count
 
 
-p = [1, 7, 6, 5, 4, 3, 2]
-merge_sort(p)
+def merge_sort(a):
+    return merge_sort_helper(a, 0)
+
+
+p = [4, 3, 2, 1]
+print("No of inversions in the array are : "+ str(merge_sort(p)))
 print(p)
-
-
-
